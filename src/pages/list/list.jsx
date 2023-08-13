c42633c11b2537a9c12fe9d76cc06962fcbcbf31
RachaManideep <racha20231@iiitd.ac.in>
2023-08-13 23:13:16 +0530

    sdfgs

--- src/pages/list/list.jsx
+++ src/pages/list/list.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import "./list.css"
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 
 const list = () => {
   return (
     <div>
-        <Navbar/><Header type = 'list'/>
+        <Navbar/>
+        <Header type = 'list'/>
+        <div className="listContainer">
+          <div className="listWrapper">
+            <div className="listSearch">
+              <h1 className="lsTitile">Search</h1>
+            </div>
+            <div className="listresult"></div>
+          </div>
+        </div>
     </div>
   )
 }
